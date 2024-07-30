b6c05f6499f9201cee073c15755ed3665a12f0d9
jash707 <jash.tamakuwala777@gmail.com>
2024-07-30 17:09:14 +0530

    implemented GetItemFromCart

--- controllers/address.go
+++ controllers/address.go
@@ -25,9 +25,9 @@ func EditWorkAddress() gin.HandlerFunc {
 
 func DeleteAddress() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		userID := c.Query("id")
+		user_id := c.Query("id")
 
-		if userID == "" {
+		if user_id == "" {
 			c.Header("Content-Type", "application/json")
 			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid search index"})
 			c.Abort()
@@ -35,14 +35,14 @@ func DeleteAddress() gin.HandlerFunc {
 		}
 
 		addresses := make([]models.Address, 0)
-		user_id, err := primitive.ObjectIDFromHex(userID)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
 		if err != nil {
 			c.IndentedJSON(http.StatusInternalServerError, "Internal Server Error")
 		}
 
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
-		filter := bson.D{primitive.E{Key: "_id", Value: user_id}}
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
 		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
 		_, err = UserCollection.UpdateOne(ctx, filter, update)
 		if err != nil {

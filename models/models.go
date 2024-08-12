455fedf033a01c7f27c72651eea2cf3e91eb192f
jash707 <jash.tamakuwala777@gmail.com>
2024-08-12 15:19:28 +0530

    type correction ProductUser

--- models/models.go
+++ models/models.go
@@ -34,7 +34,7 @@ type Product struct {
 type ProductUser struct {
 	Product_ID   primitive.ObjectID `bson:"_id"`
 	Product_Name *string            `json:"product_name" bson:"product_name"`
-	Price        *uint64            `json:"price" bson:"price"`
+	Price        int                `json:"price" bson:"price"`
 	Rating       *uint8             `json:"rating" bson:"rating"`
 	Image        *string            `json:"image" bson:"image"`
 }
